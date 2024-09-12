4451227309f65c8721b423eb14f95f7cb9c13009
bastien <bastienjouffre@ynov.com>
2024-09-12 10:21:41 +0200

    test 1

--- src/Menus/Inventaire.go
+++ src/Menus/Inventaire.go
@@ -1,4 +1,4 @@
-package main
+package Menus
 
 import "fmt"
 
---
+++ src/Menus/Menu.go
@@ -0,0 +1,30 @@
+package Menus
+
+import (
+	"fmt"
+	"os"
+)
+
+func Menu() {
+	var ans int
+	for {
+		fmt.Println("Choisissez ce que vous voulez faire.")
+		fmt.Println("1.Regarder votre Inventaire.")
+		fmt.Println("2.Regarder vos Stats.")
+		fmt.Println("3.Quitter.")
+		_, err := fmt.Scan(&ans)
+		if err != nil {
+			return
+		}
+		switch ans {
+		case '1':
+			fmt.Println("Vous regarder votre Inventaire.")
+		case '2':
+			fmt.Println("Vous regarder Vos Stats.")
+		case '3':
+			fmt.Println("J'espère à bientôt !")
+			os.Exit(1)
+
+		}
+	}
+}
