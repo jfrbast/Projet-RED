f8f5eca4cf9cd63eec33995c9238e5a3a2d8a418
nans <nans.moll@ynov.com>
2024-09-12 11:58:11 +0200

    test

--- src/Menus/Inventaire.go
+++ src/Menus/Inventaire.go
@@ -2,11 +2,32 @@ package Menus
 
 import "fmt"
 
-func Inventaire{
-
+type Item struct {
+	Name string
 }
 
+func ShowInventory() {
 
+	inventory := []Item{
+		{Name: "casque"},
+		{Name: "placetron"},
+		{Name: "Jambieres"},
+		{Name: "bottes"},
+		{Name: "potions de soins"},
+		{Name: "potions de poisons"},
+		{Name: ""},
+		{Name: ""},
+		{Name: ""},
+		{Name: ""},
+	}
 
+	for i, item := range inventory {
+		if item.Name != "" {
+			fmt.Printf("Item %d: %s\n", i, item.Name)
+		} else {
+			fmt.Printf("Item %d: Vide\n", i)
+		}
+	}
+}
 
 //1. Tableau dont la taille ne varie pas 10 max  + Si inventaire est vide print vide sinon print tout l inventaire
