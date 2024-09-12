820f0ad75a26bc46db93fee717c1f6f890716955
bastien <bastienjouffre@ynov.com>
2024-09-12 15:00:04 +0200

    test

--- src/Menus/Menu.go
+++ src/Menus/Menu.go
@@ -12,17 +12,17 @@ func Menu() {
 		color.Blue("Choisissez ce que vous voulez faire.")
 		fmt.Println("1.Regarder votre Inventaire.")
 		fmt.Println("2.Regarder vos Stats.")
-		fmt.Println("3.Quitter.")
+		color.Red("3.Quitter")
 		_, err := fmt.Scan(&ans)
 		if err != nil {
 			return
 		}
 		switch ans {
-		case '1':
+		case 1:
 			fmt.Println("Vous regarder votre Inventaire.")
-		case '2':
+		case 2:
 			fmt.Println("Vous regarder Vos Stats.")
-		case '3':
+		case 3:
 			fmt.Println("J'espère à bientôt !")
 			os.Exit(1)
 		default:
--- src/exec/main.go
+++ src/exec/main.go
@@ -5,6 +5,6 @@ import (
 )
 
 func main() {
-	projetred.ShowInventory()
+	projetred.Menu()
 
 }
